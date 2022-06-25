e9b370051519e3856cf7694ee8a530a278d3d4b5
MohammadrezaQaderi <Mrq112775@outlook.com>
2022-06-25 20:53:51 +0430

    thE PING type of message

--- server.py
+++ server.py
@@ -67,3 +67,21 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             elif len(data.decode()) > 0 and "Ping" not in data.decode():
                 print(data.decode())
                 #time.sleep(40)
+    elif message_type == 'Ping':
+        final_message = ""
+        final_message += message_type
+        #time.sleep(20)
+        s.sendall(final_message.encode())
+        current_time = time.time()
+        while True:
+            try:
+                data = s.recv(1024)
+            except Exception:
+                print("unsuccessful, your connection has been closed")
+                break
+            if data.decode() == "Pong":
+                print(f"{HOST}, {PORT} sent {data.decode()} successfully")
+                # time.sleep(50)
+            elif len(data.decode()) > 0:
+                final_message = "Pong"
+                s.sendall(final_message.encode())
