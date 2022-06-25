81dd0c07bd0324f2ec69ddfff2b860c27e113b39
MohammadrezaQaderi <Mrq112775@outlook.com>
2022-06-25 20:49:43 +0430

    thE publish type of message

--- server.py
+++ server.py
@@ -16,3 +16,23 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         exit()
 
     print(f"connected successfully to {HOST}, {PORT}")
+    message_type = sys.argv[3]
+    if message_type == 'Publish':
+        final_message = ""
+        for i in range(3, len(sys.argv)):
+            final_message += sys.argv[i]
+            final_message += " "
+        s.sendall(final_message.encode())
+        while True:
+            try:
+                data = s.recv(1024)
+            except Exception:
+                print("unsuccessful, your connection has been closed")
+                break
+            if data.decode() == 'Ping':
+                final_message = "Pong"
+                s.sendall(final_message.encode())
+            elif len(data.decode()) > 0:
+                print("your message published successfully")
+                # if "PubAck" in data.decode():
+                #     print(data.decode())
