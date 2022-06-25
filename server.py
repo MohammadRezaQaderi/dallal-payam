701dfba53c9eeee59686b0c4677ffa8c82ee97f3
MohammadrezaQaderi <Mrq112775@outlook.com>
2022-06-25 20:52:54 +0430

    thE سعذسزقهذث type of message

--- server.py
+++ server.py
@@ -6,6 +6,12 @@ import time
 HOST = sys.argv[1]
 PORT = int(sys.argv[2])
 
+def subscribe_request(conn, message):
+    try:
+        conn.sendall(message.encode())
+    except Exception:
+        print("failed")
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     try:
         print(HOST, PORT)
@@ -36,3 +42,28 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 print("your message published successfully")
                 # if "PubAck" in data.decode():
                 #     print(data.decode())
+    elif message_type == 'Subscribe':
+        threads = []
+        for i in range(4, len(sys.argv)):
+            final_message = message_type
+            final_message += " "
+            final_message += sys.argv[i]
+            t = threading.Thread(target=subscribe_request, args=(s, final_message))
+            threads.append(t)
+        #time.sleep(20)
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
+        while True:
+            try:
+                data = s.recv(1024)
+            except Exception:
+                print("unsuccessful, your connection has been closed")
+                break
+            if data.decode() == 'Ping':
+                final_message = "Pong"
+                s.sendall(final_message.encode())
+            elif len(data.decode()) > 0 and "Ping" not in data.decode():
+                print(data.decode())
+                #time.sleep(40)
