95be3a1738cd8433a40a2100c1b5f4bba1f2aab2
MohammadrezaQaderi <Mrq112775@outlook.com>
2022-06-25 20:56:34 +0430

    the server initalization

---
+++ server.py
@@ -0,0 +1,6 @@
+import socket
+import threading
+import time
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 1378  # Port to listen on (non-privileged ports are > 1023)
